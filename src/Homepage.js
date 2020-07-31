01eb828020d9f3e25eeb96c470e4cdf1ff9afe00
Rohitdutt <parsairohit2@gmail.com>
2020-07-31 23:34:00 +0530

    added hover effect on images

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,40 +1,10 @@
 import React from "react";
 import "./Homepage.scss";
+import Directory from "./components/directory/directory";
 
 const homePage = () =>(
     <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>HATS</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>JACKETS</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>SNEAKERS</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>WOMENS</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>MENS</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-            </div>
+         <Directory/>
     </div>
 )
 

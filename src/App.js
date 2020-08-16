7324fa060f9804a64c255443073775ba96e0562e
Rohitdutt <parsairohit2@gmail.com>
2020-08-16 23:24:19 +0530

    added firebase configs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import SignInAndSignUpPage from "./components/pages/sign-in and sign-up/sign-in and sign-up";
 import {Switch,Route} from "react-router-dom";
 import HomePage from "./components/pages/homepage/Homepage";
 import ShopPage from "./components/pages/shop/shop";
 import Header from "./components/header/header";
+import {auth} from "./firebase/firebase";
 
-function App() {
-  return (
-    <div >
-        <Header/>
-        <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUpPage}/>
-        </Switch>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state={
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+            this.setState({currentUser: user});
+            console.log(user)
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser }/>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                    <Route path='/signin' component={SignInAndSignUpPage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/custom-button/custom-button.js
+++ src/components/custom-button/custom-button.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./custom-button.scss";
 
-const CustomButton = ({children,...otherProps}) =>(
+const CustomButton = ({children,isGoogleSignIn,...otherProps}) =>(
     <button className="custom-button" {...otherProps}>
         {
             children

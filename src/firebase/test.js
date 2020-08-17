30643b1691a3b521f6c52a14ad1757a3bc2ae0c1
Rohitdutt <parsairohit2@gmail.com>
2020-08-17 20:33:03 +0530

    added firebase configs

---
+++ src/firebase/test.js
@@ -0,0 +1,8 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firestore = firebase.firestore();
+
+firestore.collection("users").doc("70n2zuyKu7p1JZv0OWvS").collection("cartItems").doc("rCSUEwVDQZJmc0crkVO7");
+firestore.doc("/users/70n2zuyKu7p1JZv0OWvS/cartItems/rCSUEwVDQZJmc0crkVO7");
+firestore.collection("/users/70n2zuyKu7p1JZv0OWvS/cartItems");

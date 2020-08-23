59968c7265a5326ff5dbf4302dd35e760696c4d7
Rohitdutt <parsairohit2@gmail.com>
2020-08-23 23:44:09 +0530

    if user is signed than we can not access login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import SignInAndSignUpPage from "./components/pages/sign-in and sign-up/sign-in and sign-up";
-import {Switch,Route} from "react-router-dom";
+import {Switch,Route,Redirect} from "react-router-dom";
 import HomePage from "./components/pages/homepage/Homepage";
 import ShopPage from "./components/pages/shop/shop";
 import Header from "./components/header/header";
@@ -43,15 +43,19 @@ class App extends Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
-                    <Route path='/signin' component={SignInAndSignUpPage}/>
+                    <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
                 </Switch>
             </div>
         );
     }
 }
 
+const mapStateToProps = ({user}) =>({
+    currentUser: user.currentUser
+})
+
 const mapDispatchToProps = (dispatch) => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
